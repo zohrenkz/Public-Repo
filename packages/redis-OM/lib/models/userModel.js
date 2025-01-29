c8b7b5588a25aa483d205447a0b92ed188f8f355
zohrenkz <33452838+zohrenkz@users.noreply.github.com>
2025-01-29 17:47:43 +0330

    Delete packages directory

--- packages/redis-OM/lib/models/userModel.js
+++
@@ -1,6 +0,0 @@
-import { Schema } from "redis-om";
-export const userSchema = new Schema("User", {
-    firstName: { type: "string" },
-    lastName: { type: "string" },
-    email: { type: "string" },
-});
--- packages/redis-library/lib/config/enums.js
+++
@@ -1,8 +0,0 @@
-export var PersistenceMode;
-(function (PersistenceMode) {
-    PersistenceMode["NONE"] = "none";
-    PersistenceMode["RDB"] = "rdb";
-    PersistenceMode["AOF"] = "aof";
-})(PersistenceMode || (PersistenceMode = {}));
-;
-;
